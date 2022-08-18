35237c425d9f52224c3f85bf075770330d5ce2ee
clyang82 <chuyang@redhat.com>
2022-08-18 10:29:11 +0800

    update the label name
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- controllers/propagator/policyMapper.go
+++ controllers/propagator/policyMapper.go
@@ -55,7 +55,7 @@ func policyMapper(c client.Client) handler.MapFunc {
 			name = object.GetName()
 			namespace = object.GetNamespace()
 
-			if _, ok := object.GetLabels()["hub-of-hubs.open-cluster-management.io/local-resource"]; !ok {
+			if _, ok := object.GetLabels()["global-hub.open-cluster-management.io/local-resource"]; !ok {
 				log.V(2).Info("Found a global policy, skipping it")
 
 				return nil
